5fe3b76590e72b7f1c4fe03791d89df8bc7e7bac
zakaria1293 <zakaria@depannologue.fr>
2016-06-20 11:48:46 +0200

    modification selon PR

--- app/services/notify_by_s_m_s_service.rb
+++ app/services/notify_by_s_m_s_service.rb
@@ -1,4 +1,8 @@
 class NotifyBySMSService
+    def self.perform(to, message){
+        new.perform(to, message)
+    }
+
     def perform(to, message)
         twilio_client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
         twilio_client.account.messages.create({
@@ -8,8 +12,4 @@ class NotifyBySMSService
         })
         new.perform(to, message)
     end
-
-    def self.perform(to, message){
-        new.perform(to, message)
-    }
 end
