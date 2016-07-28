92585f0bf99bb1a359bb4c5b5ad3b456fd642038
Benjamin Descamps <userbn@linux.home>
2016-07-29 01:41:14 +0200

    update design

--- app/controllers/client/profession_controller.rb
+++ app/controllers/client/profession_controller.rb
@@ -1,9 +1,7 @@
 class Client::ProfessionController < ApplicationController
   def show
     session[:profession_id] = params[:profession]
-    profession = Profession.find_by_slug!(params[:profession])
-    @intervention_parents = profession.intervention_type.where(parent_id: nil)
+    @profession = Profession.find_by_slug!(params[:profession])
+    @intervention_parents = @profession.intervention_types.where(parent_id: nil)
   end
-
-
 end
