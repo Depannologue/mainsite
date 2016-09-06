bb7e598c85c509ca3f55b4693622b8bff5fe80b4
Henri <HenriDepannologue>
2016-09-06 11:27:07 +0200

    client/app.js -> pro/app.js edit layout

--- app/assets/javascripts/client/application.js
+++ app/assets/javascripts/client/application.js
@@ -1,9 +1,6 @@
 //= require jquery
 //= require foundation
-//= require slick.js
-//= require ./swipetabs
+
 jQuery(document).ready(function($){
     $(document).foundation();
 });
-
-swipeTabsInit();
