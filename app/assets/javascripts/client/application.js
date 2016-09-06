c1a067c40269d081230fdae1e865f17954d3f6e5
Henri <HenriDepannologue>
2016-09-06 11:17:39 +0200

    edit application.js require

--- app/assets/javascripts/client/application.js
+++ app/assets/javascripts/client/application.js
@@ -1,7 +1,7 @@
 //= require jquery
 //= require foundation
 //= require slick.js
-//= require client/swipetabs.js
+//= require ./swipetabs
 jQuery(document).ready(function($){
     $(document).foundation();
 });
