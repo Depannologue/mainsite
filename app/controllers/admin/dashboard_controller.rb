d95919ecb618a6c3febc2430b87f6fcea698d8b8
Henri <HenriDepannologue>
2016-09-12 17:35:01 +0200

    add Table

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -7,10 +7,9 @@ class Admin::DashboardController < ApplicationController
   def index
     interventions = Intervention.all
 
-    @revenues = 0
-    interventions.each do |intervention|
-      @revenues += intervention.price
-    end
+    @total_number_of_interventions = interventions.count
+    @total_revenues = interventions.sum(:price)
+    @total_average_per_intervention = @total_number_of_interventions > 1 ? @total_revenues/@total_number_of_interventions : @total_revenues
     @professions_revenues = professions_revenues
 
   end
@@ -21,8 +20,10 @@ class Admin::DashboardController < ApplicationController
     professions = Profession.all
     revenues_by_professions = Array.new
     professions.each do |profession|
+      profession_number_of_interventions = profession.interventions.count
       profession_revenues = profession.interventions.sum(:price)
-      revenues_by_professions.push([profession.name,profession_revenues])
+      profession_average_per_intervention =  profession_number_of_interventions > 1 ? profession_revenues/profession_number_of_interventions : profession_revenues
+      revenues_by_professions.push([profession.name,profession_number_of_interventions,profession_revenues,profession_average_per_intervention])
     end
     revenues_by_professions
   end
